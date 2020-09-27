e0a0cd4e5f947dbce96d864aace281b175ca0592
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-09-27 12:01:28 +0530

    Blog backend added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const blogsRouter = require('./routes/blogs');
 
 dotenv.config();
 
@@ -11,12 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
-const connection = mongoose.connection;
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+});
 
+const connection = mongoose.connection;
 connection.once('open', () => {
   console.log('MongoDB connected!');
 });
+
+//Routes
+app.use('/blogs', blogsRouter);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
